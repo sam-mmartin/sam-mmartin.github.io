aabd1a1973cd1071e3bdb2109708584327dc0f68
sam m. martin <smeirelesmartins@gmail.com>
2017-09-08 14:25:37 -0300

    Modificado tema do header da Homepage

--- Script/styleModification.js
+++ Script/styleModification.js
@@ -220,3 +220,24 @@ function startARCO() {
     javaArco();
     pythontArco();
 }
+function scrollHeader() {
+    var scrollPosition;
+    var containerHEADER = document.querySelector('.header-paralax');
+    var containerICON = document.querySelector('.icon-paralax');
+
+    scrollPosition = window.scrollY;
+    if(scrollPosition <= 577) {
+        containerICON.style.position = "";
+        containerHEADER.style.opacity = 1 - (scrollPosition / 577);
+        containerICON.style.boxShadow = "0 0 0";
+        console.log(scrollPosition);
+        console.log(containerICON.style.transform);
+    }else {
+        containerICON.style.position = "fixed";
+        containerICON.style.top = "0";
+        containerICON.style.width = "100%";
+        containerICON.style.background = "rgba(255, 255, 255, 0.85)";
+        containerICON.style.boxShadow = "0 2px 6px #636363";        
+    }
+}
+window.addEventListener('scroll', scrollHeader);
