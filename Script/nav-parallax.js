fb35e5602b93913c3c5657e95184e50054ff3578
LordsIMPERIUM <sam.c3tech@hotmail.com>
2018-03-16 04:48:23 -0300

    Modificado estilo

--- Script/nav-parallax.js
+++ Script/nav-parallax.js
@@ -6,7 +6,7 @@ $(document).ready(function(){
       $(".navbar-parallax").addClass('white');
       $(".navbar-parallax").css('box-shadow', '0 2px 2px 0 rgba(0, 0, 0, 0.14), 0 1px 5px 0 rgba(0, 0, 0, 0.12), 0 3px 1px -2px rgba(0, 0, 0, 0.2)');
       $(".responsive-img").attr('src', 'Imagens/developer-icon-black.png');
-      $(".nav-link").css('color', '#000');
+      $(".link-parallax").css('color', '#000');
       $(".button-collapse").css('color', "rgba(0, 0, 0, 0.87)");
       $(".fab").css('color', "rgba(0, 0, 0, 0.87)");
       $(".navbar-parallax").css('transition', 'all 0.3s ease-out');
@@ -16,7 +16,7 @@ $(document).ready(function(){
       $(".navbar-parallax").addClass('transparent');
       $(".navbar-parallax").css('box-shadow', 'none');
       $(".responsive-img").attr('src', 'Imagens/developer-icon.png');
-      $(".nav-link").css('color', '#FFF');
+      $(".link-parallax").css('color', '#FFF');
       $(".button-collapse").css('color', "#FFF");
       $(".fab").css('color', "#FFF");
       $(".navbar-parallax").css('transition', 'all 0.3s ease-in');
